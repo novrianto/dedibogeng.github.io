cdc29604899d9311673b27a2c865d21799d40328
dediramadhan <dedinovrianto60@gmail.com>
2020-05-13 13:51:20 +0700

    update

--- assets/quotes.js
+++ assets/quotes.js
@@ -1,10 +1,10 @@
 // prettier-ignore
 export default [
-    { author: "avrill.v", quotes: "Whatever happens, keep breathing" },
-    { author: "antjedtta_", quotes: "Pam pam pararam ceklek jedar\nPararam ram parararam ram" },
+    { author: "avrill.v", quotes: "Mangadd Kuliahnyaaa" },
+    { author: "antjedtta_", quotes: "Ciee Stalkerrr :v" },
     { author: "standinshd", quotes: "Hey! I wuv chuuuu! UwU" },
-    { author: "standinshd", quotes: "I love you." },
-    { author: "standinshd", quotes: "I may not with you everyday, but I love you everyday." },
-    { author: "standinshd", quotes: "I love you." },
+    { author: "standinshd", quotes: "When love is not madness, it is not love." },
+    { author: "standinshd", quotes: "Falling in love is awfully simple, but falling out of love is simply awful." },
+    { author: "standinshd", quotes: "bjirr abis kata-kata" },
     { author: "standinshd", quotes: "I'm sorry for loving you." },
 ];
